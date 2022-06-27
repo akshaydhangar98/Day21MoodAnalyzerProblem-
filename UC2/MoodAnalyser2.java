df38eb3e0a836916b56bdacd2a389b3acd8041d5
akshay dhangar <akshaygdhangar14@gmail.com>
2022-06-27 16:42:02 +0530

    UC_2

---
+++ UC2/MoodAnalyser2.java
@@ -0,0 +1,13 @@
+
+public class MoodAnalyser2 {
+
+	public MoodAnalyser2(Object object) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String analyseMood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
